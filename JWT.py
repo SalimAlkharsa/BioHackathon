32ac20f45e9f87214db80587bf9b7463ce4e8d24
Salimalkharsa <salimalkharsa@gmail.com>
2023-05-30 11:37:21 -0500

    Made some changes to the JWT.py file. It now works better

--- JWT.py
+++ JWT.py
@@ -1,6 +1,7 @@
 import jwt
 import requests
-from datetime import datetime, timedelta
+import time
+import uuid
 
 # This is setting up the project as a an EPIC backend service.
 # The docs https://fhir.epic.com/Documentation?docId=oauth2&section=BackendOAuth2Guide
@@ -12,45 +13,44 @@ with open("privatekey.pem", "r") as file:
 # Define the JWT headers
 headers = {
     "alg": "RS384",
-    "typ": "JWT"
+    "typ": "JWT",
+    "kid": ""  # IDK what this is
 }
 
 # Define the JWT payload
-payload = {
-    # client_id -> just a test client_id not real
+# Construct JWT claims
+now = time.time()
+claims = {
     "iss": "34039589-fc11-4319-bba9-4ba65bdc684a",
-    "sub": "d45049c3-3441-40ef-ab4d-b9cd86a17225",  # ?? maybe client_id
+    "sub": "34039589-fc11-4319-bba9-4ba65bdc684a",
     "aud": "https://fhir.epic.com/interconnect-fhir-oauth/oauth2/token",
-    "jti": "f9eaafba-2e49-11ea-8880-5ce0c5aee679",  # This should not be hardcoded
-    # Max is 5 minutes
-    "exp": int((datetime.utcnow() + timedelta(minutes=5)).timestamp()),
+    "jti": str(uuid.uuid4()),  # fill with reference id
+    "exp": int(now + 1 * 60)  # cannot be more than 5 minutes!
 }
 
-# Generate the JWT
-jwt_token = jwt.encode(payload, private_key,
-                       algorithm="RS384", headers=headers)
+# Generate signed token using private key with RS384 algorithm
+# Replace with the path to your private key file
+signing_key = open("privatekey.pem").read()
+signed_token = jwt.encode(claims, signing_key, algorithm="RS384")
 
-# Print the JWT
-print(jwt_token.encode())
+# print("  => signed token:", signed_token)
 
-
-# Define the token endpoint URL
-token_endpoint = "https://fhir.epic.com/interconnect-fhir-oauth/oauth2/token"
-
-# Define the form-urlencoded parameters
+# Prepare API call payload
 payload = {
     "grant_type": "client_credentials",
     "client_assertion_type": "urn:ietf:params:oauth:client-assertion-type:jwt-bearer",
-    "client_assertion": jwt_token.encode()
+    "client_assertion": signed_token.encode("utf-8"),
 }
 
-# Send the POST request
-# There is a prior error bc this does not return what is expected
-response = requests.post(token_endpoint, data=payload)
+# Make API call
+oauth2TokenUrl = "https://fhir.epic.com/interconnect-fhir-oauth/oauth2/token"
+# Dispatch the API call
+response = requests.post(oauth2TokenUrl, data=payload)
 
-# Check the response status code and content
-if response.status_code == 200:
-    access_token = response.json().get("access_token")
-    print("Access Token:", access_token)
-else:
-    print("Error:", response.text)
+# Check for errors
+response.raise_for_status()
+
+# Print response details
+print("  => response status:", response.status_code)
+print("  => response header:", response.headers)
+print("  => response body:", response.text)
