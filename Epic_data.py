c43a8eb9ebcaa209b98b7b04a74b88c2c45ef8f5
Salimalkharsa <salimalkharsa@gmail.com>
2023-06-03 00:58:39 -0500

    Can now pull the data into a Txt file

--- Epic_data.py
+++ Epic_data.py
@@ -3,17 +3,13 @@ import requests
 import json
 import time
 import uuid
+import xml.etree.ElementTree as ET
 
 # Set up a patient object
 
 
 class Patient:
     def __init__(self, patient_ID):
-        # Define the JWT headers
-        '''self.headers = {"alg": "RS384",
-                        "typ": "JWT",
-                        "kid": ""  # IDK what this is
-                        }'''
         # Helper Getter variables
         self.token = "Bearer " + self.get_access_token()
         self.base_url = "https://fhir.epic.com/interconnect-fhir-oauth/api/FHIR/DSTU2/"
@@ -24,27 +20,154 @@ class Patient:
         self.patientData = self.get_patientData()
         self.allergyIntolerance = self.get_allergyIntolerance()
         self.condition = self.get_condition()
+        self.diagnostics = self.get_Diagnostics()
+        self.goal = self.get_Goal()
+        self.medication = self.get_Medication()
+        self.medicationOrder = self.get_MedicationOrder()
+        self.medicationStatement = self.get_MedicationStatement()
+        self.observation = self.get_Observation()
 
-    # Getters
+    # Basic Getters
+
     def get_patientData(self):
         headers = {
             "Authorization": self.token
         }
         return requests.get(self.base_url + "Patient/" + self.patient_ID, headers=headers)
 
+    def get_resourceID(self, resource_name):
+        headers = {
+            "Authorization": self.token
+        }
+        if resource_name == "Observation":
+            url = self.base_url + resource_name + "?patient=" + self.patient_ID + "&category=social-history&issued={issued}"
+        else:
+            url = self.base_url + resource_name + "?patient=" + self.patient_ID
+        response = requests.get(url, headers=headers)
+        root = ET.fromstring(response.text)
+
+        # Define the namespace
+        namespace = {'fhir': 'http://hl7.org/fhir'}
+
+        # Extract the ID values
+        id_values = []
+        entries = root.findall(".//fhir:entry", namespace)
+        for entry in entries:
+            resource = entry.find(".//fhir:resource", namespace)
+            if resource is not None:
+                id_element = resource.find(".//fhir:id", namespace)
+                if id_element is not None:
+                    id_value = id_element.attrib['value']
+                    id_values.append(id_value)
+
+        return id_values
+
+    # Data Getters
+
     def get_allergyIntolerance(self):
         headers = {
             "Authorization": self.token
         }
-        return requests.get(self.base_url + "AllergyIntolerance/" + self.patient_ID, headers=headers)
+        rescIDs = self.get_resourceID("AllergyIntolerance")
+        info = []
+        for rescID in rescIDs:
+            info.append(requests.get(self.base_url +
+                        "AllergyIntolerance/" + rescID, headers=headers).text)
+        return info
 
     def get_condition(self):
         headers = {
             "Authorization": self.token
         }
-        return requests.get(self.base_url + "Condition/" + self.patient_ID, headers=headers)
+        rescIDs = self.get_resourceID("Condition")
+        info = []
+        for rescID in rescIDs:
+            info.append(requests.get(self.base_url +
+                        "Condition/" + rescID, headers=headers).text)
+        return info
+
+    def get_Diagnostics(self):
+        headers = {
+            "Authorization": self.token
+        }
+        rescIDs = self.get_resourceID("DiagnosticReport")
+        info = []
+        for rescID in rescIDs:
+            info.append(requests.get(self.base_url +
+                        "DiagnosticReport/" + rescID, headers=headers).text)
+        return info
+
+    def get_Goal(self):
+        headers = {
+            "Authorization": self.token
+        }
+        rescIDs = self.get_resourceID("Goal")
+        info = []
+        for rescID in rescIDs:
+            info.append(requests.get(self.base_url +
+                        "Goal/" + rescID, headers=headers).text)
+        return info
+
+    def get_Medication(self):
+        headers = {
+            "Authorization": self.token
+        }
+        rescIDs = self.get_resourceID("Medication")
+        info = []
+        for rescID in rescIDs:
+            info.append(requests.get(self.base_url +
+                        "Medication/" + rescID, headers=headers).text)
+        return info
+
+    def get_MedicationOrder(self):
+        headers = {
+            "Authorization": self.token
+        }
+        rescIDs = self.get_resourceID("MedicationOrder")
+        info = []
+        for rescID in rescIDs:
+            info.append(requests.get(self.base_url +
+                        "MedicationOrder/" + rescID, headers=headers).text)
+        return info
+
+    def get_MedicationStatement(self):
+        headers = {
+            "Authorization": self.token
+        }
+        rescIDs = self.get_resourceID("MedicationStatement")
+        info = []
+        for rescID in rescIDs:
+            info.append(requests.get(self.base_url +
+                        "MedicationStatement/" + rescID, headers=headers).text)
+        return info
+
+    def get_Observation(self):
+        headers = {
+            "Authorization": self.token
+        }
+        rescIDs = self.get_resourceID("Observation")
+        info = []
+        for rescID in rescIDs:
+            info.append(requests.get(self.base_url +
+                        "Observation/" + rescID, headers=headers).text)
+        return info
+
+    # Get the data in a JSON format
+    def to_text(self):
+        patient_text = f"Patient ID: {self.patient_ID}\n"
+        patient_text += f"Patient Data:\n{self.patientData.text}\n"
+        patient_text += f"Allergy Intolerance:\n{self.allergyIntolerance}\n"
+        patient_text += f"Condition:\n{self.condition}\n"
+        patient_text += f"Diagnostics:\n{self.diagnostics}\n"
+        patient_text += f"Goal:\n{self.goal}\n"
+        patient_text += f"Medication:\n{self.medication}\n"
+        patient_text += f"Medication Order:\n{self.medicationOrder}\n"
+        patient_text += f"Medication Statement:\n{self.medicationStatement}\n"
+        patient_text += f"Observation:\n{self.observation}\n"
+        return patient_text
 
     # Establish a connection to the API
+
     def generate_signed_token(self):
         now = time.time()
         claims = {
@@ -72,8 +195,10 @@ class Patient:
 
 
 ### TESTING###
-patID = "TnHWKG1c3Kc571xwwVjTuGQB"
-pat = Patient(patID)
+patID = "erXuFYUfucBZaryVksYEcMg3"
+patient = Patient(patID)
 # '''
-print(pat.condition.text)
+x = patient.to_text()
+with open('x.txt', 'w') as file:
+    file.write(x)
 # '''
