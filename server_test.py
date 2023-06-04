a7441fc46c993c02432c946f87c596ced000581d
Salimalkharsa <salimalkharsa@gmail.com>
2023-06-04 01:29:24 -0500

    just does a post request on the api to verify it works (local)

---
+++ server_test.py
@@ -0,0 +1,22 @@
+import requests
+
+# Set the API endpoint URL
+url = 'http://127.0.0.1:5000/cds'
+
+# Example patient ID
+patient_id = 'eIXesllypH3M9tAA5WdJftQ3'
+
+# Create the request payload
+payload = {'patient_id': patient_id}
+
+# Send the POST request to the API endpoint
+response = requests.post(url, json=payload)
+
+# Check the response status code
+if response.status_code == 200:
+    # Parse the JSON response
+    data = response.json()
+    cds_output = data['cds_output']
+    print('CDS Output:', cds_output)
+else:
+    print('Error:', response.status_code)
